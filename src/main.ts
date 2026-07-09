a2f93fc7b82f76eb46878ca9337a82ce5fa3e57b
muthu7550 <muthu3861406@gmail.com>
2026-07-09 13:34:41 +0530

    Fix NestJS Vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import serverlessExpress from '@codegenie/serverless-express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-   app.enableCors();
-  await app.listen( 5000);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.enableCors();
+
+  await app.init();
+
+  return serverlessExpress({ app: server });
 }
-bootstrap();
+
+export default bootstrap();
