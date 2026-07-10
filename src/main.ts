91b62e73d77a755a12c42edb63e8fee3c82da32c
muthu7550 <muthu3861406@gmail.com>
2026-07-10 12:35:40 +0530

    worked on the issue

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);                                                                           
-app.enableCors({
-  origin: [
-    'https://front-end-ui-for-nest-6nibg9x4d-muthu7550s-projects.vercel.app',
-    'http://localhost:3000',
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-});
-  await app.listen( 5000);
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://front-end-ui-for-nest-jmptgdrbb-muthu7550s-projects.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 5000);
 }
+
 bootstrap();
